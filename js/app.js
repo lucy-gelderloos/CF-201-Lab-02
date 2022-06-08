791450ea34e371e8d3ae0ca1087d1769b3b7bf90
Lucy Gelderloos <lucygel@gmail.com>
2022-06-07 18:59:47 -0700

    Add last two questions

--- js/app.js
+++ js/app.js
@@ -32,11 +32,29 @@ function guessingGame() {
 
   let bornIn = prompt('I currently live in Seattle. Did I grow up there?').toLowerCase();
   if(bornIn === 'no'){
-    console.log('Question 2 correct');
+    console.log('Question 3 correct');
     alert('Correct!');
   } else {
-    console.log('Question 2 incorrect');
+    console.log('Question 3 incorrect');
     alert('Incorrect - I grew up in Colorado.');
+  }
+
+  let owMyBones = prompt('I broke my arm when I was 7.').toLowerCase();
+  if(owMyBones === 'no'){
+    console.log('Question 4 correct');
+    alert('Correct! I never broke any bones until I broke my arm when I was 27.');
+  } else {
+    console.log('Question 4 incorrect');
+    alert('Incorrect');
+  }
+
+  let fireScience = prompt('For my fifth grade science project, I lit our yard on fire and got in lots of trouble.').toLowerCase();
+  if(fireScience === 'no'){
+    console.log('Question 5 correct');
+    alert('Correct! I did light our yard on fire for science, but it was under trained adult supervision.');
+  } else {
+    console.log('Question 5 incorrect');
+    alert('Incorrect! I did light our yard on fire for science, but it was under trained adult supervision.');
   }
 
   alert('Thanks for playing, ' + visitorName + '!');
